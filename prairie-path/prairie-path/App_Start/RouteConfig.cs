9f71b53da74cb6567d4b942e9a0abf1f2b429a2a
tosca ragnini <tosca.ragnini@gmail.com>
2016-01-11 22:56:15 -0600

    resend restsharp

--- prairie-path/prairie-path/App_Start/RouteConfig.cs
+++ prairie-path/prairie-path/App_Start/RouteConfig.cs
@@ -12,12 +12,17 @@ namespace prairie_path
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                "Index",
+                "Index",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
 
-          //  routes.MapRoute(
-            //    name: "Default",
-             //   url: "{controller}/{action}/{id}",
-              //  defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-          //  );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
         }
     }
 }
